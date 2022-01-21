fafdaf002d2fc2bc123e8b72fb8944f909f194e0
Raden Darajat Bambang Eka Gunawan <send.raden@gmail.com>
2022-01-21 14:40:21 +0800

    Add project files.

---
+++ Strings not always work/Kartu.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Strings_not_always_work
+{
+    class Kartu
+    {
+        public string Value { get; set; }
+
+        public string Suit { get; set; }
+
+        public string Name
+        {
+            get { return $"{this.Value} of {this.Suit}"; }
+        }
+
+
+        //constructor
+        public Kartu (string value, string suit)
+        {
+            this.Value = value;
+            this.Suit = suit;
+        }
+
+    }
+}
---
+++ Strings not always work/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Strings_not_always_work
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Kartu aceOfSpades = new Kartu("Ace", "Spades");
+            Console.WriteLine(aceOfSpades);
+
+            Console.Write("aceOfSpades.Name = ");
+            Console.WriteLine(aceOfSpades.Name);
+
+            Console.Write("aceOfSpades.Suit = ");
+            Console.WriteLine(aceOfSpades.Suit);
+
+
+        }
+    }
+}
